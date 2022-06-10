134aeb797953d58797f7a46433baf5f36d285096
王欣磊 <wangxinlei_work@foxmail.com>
2022-06-10 14:34:45 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 王欣磊
  * @Date: 2022-06-10 09:53:40
  * @LastEditors: 王欣磊
- * @LastEditTime: 2022-06-10 14:26:05
+ * @LastEditTime: 2022-06-10 14:34:29
  * @Description: 
  * @FilePath: /mmsi-demo-v3/vite.config.ts
  */
@@ -12,7 +12,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './mid-data-set/',
+  base: './',
   build:{
     outDir: 'docs',
   }
