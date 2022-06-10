26127f9d03a2bad14975d71f87a9cc089a4fd650
王欣磊 <wangxinlei_work@foxmail.com>
2022-06-10 14:08:21 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/*
+ * @Author: 王欣磊
+ * @Date: 2022-06-10 09:53:40
+ * @LastEditors: 王欣磊
+ * @LastEditTime: 2022-06-10 14:08:00
+ * @Description: 
+ * @FilePath: /mmsi-demo-v3/vite.config.ts
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build:{
+    outDir: 'doc',
+  }
 })
